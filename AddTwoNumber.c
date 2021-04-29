31f76899e2873c323edfd1b91c0b167705388334
vikasdoddamani <83420516+vikasdoddamani@users.noreply.github.com>
2021-04-30 00:43:05 +0530

    C program to add two number
    + is a Arithmetic Operator in c program
    a b Sum are a variable that store values in memory

---
+++ AddTwoNumber.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+int main()
+
+{
+  
+
+int a=10;
+     // store 10 Value in a variable
+int b=20;
+     // store 20 Value in b variable
+int sum;
+  
+    // calculate 10 and 20 using Arithmetic Operator and store in 		sum variable
+
+sum=a+b;
+  
+
+  
+printf("the value in a=%d \n", a);
+  
+printf("the value in b=%d \n", b);
+   
+
+printf("the total sum is=%d \n",sum);
+
+     // display a final message
+
+return 0;
+
+
+}
